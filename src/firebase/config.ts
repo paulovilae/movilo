8536b174f7981c44ff8293f01896c2d64a4be88e
Paulo Vila <vilapaulo@gmail.com>
2025-10-20 02:38:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2057881781-b89c3",
+  "appId": "1:644199226545:web:a777d145265eecfa25d690",
+  "apiKey": "AIzaSyA7yIVK8_JG918owLqoQQI5Bmrnxl2OjTk",
+  "authDomain": "studio-2057881781-b89c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "644199226545"
+};
